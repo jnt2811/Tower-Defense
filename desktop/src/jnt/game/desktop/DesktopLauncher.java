d99a888b79bda4a0e336781c27b8333527c6577c
Tran Quang Nguyen <tranquangnguyen2811@gmail.com>
2019-11-06 19:00:26 +0700

    sua lai nhieu thu

--- desktop/src/jnt/game/desktop/DesktopLauncher.java
+++ desktop/src/jnt/game/desktop/DesktopLauncher.java
@@ -2,14 +2,13 @@ package jnt.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import jnt.game.TowerDefense;
+import jnt.game.Main.TowerDefense;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1920;
-		config.height = 1080;
-		config.fullscreen = true;
 		new LwjglApplication(new TowerDefense(), config);
+		config.width = 1500;
+		config.height = 800;
 	}
 }
